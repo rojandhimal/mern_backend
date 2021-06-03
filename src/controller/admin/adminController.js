a369975d1d9d653fa79cb2a2ca8efceeaf9f469a
unknown <rojandhimal1@gmail.com>
2021-06-04 00:39:06 +0545

    Refactor code to reusable

--- src/controller/admin/adminController.js
+++ src/controller/admin/adminController.js
@@ -57,9 +57,3 @@ exports.signup = (req,res) =>{
         });
     };
 
-exports.requiredSignin = (req,res,next) => {
-    const token = req.headers.authorization.split(" ")[1];
-    const user = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = user
-    next();
-}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { signup, signin, requiredSignin } = require('../controller/authController');
+const { signup, signin} = require('../controller/authController');
 const { isRequestValidated, validateSignupRequest, validateSigninRequest } = require('../validators/auth');
 
 
 router.post('/signup', validateSignupRequest, isRequestValidated, signup);
 router.post('/signin',validateSigninRequest, isRequestValidated, signin);
+
 // router.post('/profile',requiredSignin,(req,res)=>{
 //     res.status(200).json({
 //         user:"profile"
